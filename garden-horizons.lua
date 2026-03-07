3ef05c9a7176fb805d91bed3993b1163e9e95403
ArchIsDead <ccryptohigh@gmail.com>
2026-03-07 20:10:05 +0700

    Update garden-horizons.lua

--- garden-horizons.lua
+++ garden-horizons.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/326e83cf49a4e251.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/8aacc14dfc31085a.lua.txt"))()
