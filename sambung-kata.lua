8630fb27e1490789b7b925ceb26519894be4e091
ArchIsDead <ccryptohigh@gmail.com>
2026-03-09 21:35:33 +0700

    Update sambung-kata.lua

--- sambung-kata.lua
+++ sambung-kata.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/6aef09f4d56900e9.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/sambung_kata_R1hcycwdsn.lua.txt"))()
