69f7b0d81cfeeab095175df48e8f96f55359853d
ArchIsDead <ccryptohigh@gmail.com>
2026-03-12 22:05:46 +0700

    Update Pterodromocurrentuc.lua

--- Pterodromocurrentuc.lua
+++ Pterodromocurrentuc.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/something_something_floats_twch_3_ELLms1xasm.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/4369879e15fcd6c0.lua.txt"))()
