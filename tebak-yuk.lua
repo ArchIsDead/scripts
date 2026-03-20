323b256e3dc0f2a6e2812e04935831b3d8704528
ArchIsDead <ccryptohigh@gmail.com>
2026-03-20 15:20:22 +0700

    Update tebak-yuk.lua

--- tebak-yuk.lua
+++ tebak-yuk.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/c7d66d0ac54181f0.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArchIsDead/scripts/refs/heads/main/5f1273df12006b22.lua.txt"))()
